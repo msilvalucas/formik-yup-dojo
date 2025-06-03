e07062609ec2fce5e2f524fa84bc1db586e984a3
Lucas Gomes <lulinux2109@gmail.com>
2025-06-03 00:58:25 -0300

    feat(Pages) create page Home

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,8 @@
+export const Home = () => {
+  return (
+    <>
+      <h1>🥋 Formik + Yup Dojo</h1>
+      <p>Repositório pessoal de práticas com Formik e Yup, organizado em formato de Dojo.</p>
+    </>
+  )
+}
